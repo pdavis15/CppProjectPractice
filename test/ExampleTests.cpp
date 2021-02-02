04c12fc7cd80ee1263f83792a897649b9717a6ac
pdavis15 <pdavis@conncoll.edu>
2021-02-01 20:56:34 -0500

    Begin TDD, create first unit test and soundex class

--- test/ExampleTests.cpp
+++ test/ExampleTests.cpp
@@ -1,33 +1,61 @@
 #include "../src/Example.hpp"
 
-#include <gtest/gtest.h>
-
-struct ExampleTests : public ::testing::Test {
-    int* x;
-
-    virtual void SetUp() override {
-        x = new int(42);
-    }
-
-    virtual void TearDown() override {
-        delete x;
-    }
+class Soundex {
 
 };
 
-// Doesnt need to match name of file, but good practice
-// Multiply accumulate test
-TEST(ExampleTests, MAC) {
-    int x = 42;
-    int y = 16;
-    int sum = 100;
-    int oldsum = sum;
-    EXPECT_EQ(
-        oldsum + x * y,
-        MAC(x,y,sum)
-    );
-    EXPECT_EQ(
-        oldsum + x * y,
-        sum
-    );
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    Soundex soundex;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Example from Youtube vid
+// struct ExampleTests : public ::testing::Test {
+//     int* x;
+
+//     virtual void SetUp() override {
+//         x = new int(42);
+//     }
+
+//     virtual void TearDown() override {
+//         delete x;
+//     }
+
+// };
+
+// // Doesnt need to match name of file, but good practice
+// // Multiply accumulate test
+// TEST(ExampleTests, MAC) {
+//     int x = 42;
+//     int y = 16;
+//     int sum = 100;
+//     int oldsum = sum;
+//     EXPECT_EQ(
+//         oldsum + x * y,
+//         MAC(x,y,sum)
+//     );
+//     EXPECT_EQ(
+//         oldsum + x * y,
+//         sum
+//     );
+// }
